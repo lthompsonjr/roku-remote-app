cd5b85294e9d6b1587ce0c09fadc3d4aa5d8e361
Simeonov, Ivan (Associate Software Developer) <ivan.simeonov@sky.uk>
2016-08-08 16:56:44 +0100

    First project commit

---
+++ HelloWorldApp/HelloWorldTest/HelloWorldTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldTest
+//
+//  Created by Simeonov, Ivan (Associate Software Developer) on 08/08/2016.
+//  Copyright © 2016 Simeonov, Ivan (Associate Software Developer). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
