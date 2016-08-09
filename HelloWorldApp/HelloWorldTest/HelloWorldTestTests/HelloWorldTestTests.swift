6bf8a61e2c808c915cf4b1360bd735a5dc41716f
Maria Diaz <maria.diaz@sky.uk>
2016-08-09 09:20:40 +0100

    Hello World User Interface Test and Unit Test (just setUp()) working properly

--- HelloWorldApp/HelloWorldTest/HelloWorldTestTests/HelloWorldTestTests.swift
+++ HelloWorldApp/HelloWorldTest/HelloWorldTestTests/HelloWorldTestTests.swift
@@ -11,9 +11,12 @@ import XCTest
 
 class HelloWorldTestTests: XCTestCase {
     
+    var vc: ViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        vc = storyboard.instantiateInitialViewController() as! ViewController
     }
     
     override func tearDown() {
@@ -21,9 +24,8 @@ class HelloWorldTestTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHelloWorldLabel() {
+    
     }
     
     func testPerformanceExample() {
--- HelloWorldApp/HelloWorldTest/HelloWorldTestUITests/HelloWorldTestUITests.swift
+++ HelloWorldApp/HelloWorldTest/HelloWorldTestUITests/HelloWorldTestUITests.swift
@@ -9,18 +9,13 @@
 import XCTest
 
 class HelloWorldTestUITests: XCTestCase {
+    
+    let app = XCUIApplication()
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
     
     override func tearDown() {
@@ -28,9 +23,13 @@ class HelloWorldTestUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testHelloWorld() {
     
+        XCTAssert(app.staticTexts["sup?"].exists)
+        XCTAssert(app.buttons["Click Me"].exists)
+        
+        app.buttons["Click Me"].tap()
+        app.tap()
+        XCTAssert(app.staticTexts["Hello World"].exists)
+    }
 }
