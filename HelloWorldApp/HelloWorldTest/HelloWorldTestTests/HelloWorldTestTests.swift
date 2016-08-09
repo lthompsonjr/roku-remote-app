89cd4c12b9eb17feec89b68e30bdcd174b521bca
Simeonov, Ivan (Associate Software Developer) <ivan.simeonov@sky.uk>
2016-08-09 10:17:49 +0100

    Some tests

--- HelloWorldApp/HelloWorldTest/HelloWorldTestTests/HelloWorldTestTests.swift
+++ HelloWorldApp/HelloWorldTest/HelloWorldTestTests/HelloWorldTestTests.swift
@@ -25,13 +25,13 @@ class HelloWorldTestTests: XCTestCase {
     }
     
     func testHelloWorldLabel() {
-    
+        XCTAssert(true);
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
-            // Put the code you want to measure the time of here.
+            XCTAssert(true);
         }
     }
     
