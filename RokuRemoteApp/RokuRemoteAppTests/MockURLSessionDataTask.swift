37aff31713413af7b63e8c02a521eacdab54bf5c
Thuringer, Nicholas (Associate Software Developer) <nicholas.thuringer@sky.uk>
2016-08-15 17:33:13 +0100

    HTTP request unit test mocking a session

---
+++ RokuRemoteApp/RokuRemoteAppTests/MockURLSessionDataTask.swift
@@ -0,0 +1,9 @@
+//
+//  MockURLSessionDataTask.swift
+//  RokuRemoteApp
+//
+//  Created by Thuringer, Nicholas (Associate Software Developer) on 15/08/2016.
+//  Copyright © 2016 Thuringer, Nicholas (Associate Software Developer). All rights reserved.
+//
+
+import Foundation
