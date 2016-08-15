e010b4d5082e507c7bfe7e581e227cf38c5500af
Thuringer, Nicholas (Associate Software Developer) <nicholas.thuringer@sky.uk>
2016-08-15 09:35:56 +0100

    Added new project for Roku Remote App

---
+++ RokuRemoteApp/RokuRemoteApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RokuRemoteApp
+//
+//  Created by Thuringer, Nicholas (Associate Software Developer) on 15/08/2016.
+//  Copyright © 2016 Thuringer, Nicholas (Associate Software Developer). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RokuRemoteApp/RokuRemoteAppTests/RokuRemoteAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  RokuRemoteAppTests.swift
+//  RokuRemoteAppTests
+//
+//  Created by Thuringer, Nicholas (Associate Software Developer) on 15/08/2016.
+//  Copyright © 2016 Thuringer, Nicholas (Associate Software Developer). All rights reserved.
+//
+
+import XCTest
+@testable import RokuRemoteApp
+
+class RokuRemoteAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
