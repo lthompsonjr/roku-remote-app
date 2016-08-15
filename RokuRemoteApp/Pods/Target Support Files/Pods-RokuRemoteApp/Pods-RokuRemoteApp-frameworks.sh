a6856c1fbe7b6f12e49b4504ef190fe83212ebc2
maria <maria@ostml0231735.bskyb.com>
2016-08-15 17:35:24 +0100

    Test return type when creating UDPSocketConnection and start implementation of Factory method pattern (UDP/TCP Sockets)

--- RokuRemoteApp/Pods/Target Support Files/Pods-RokuRemoteApp/Pods-RokuRemoteApp-frameworks.sh
+++ RokuRemoteApp/Pods/Target Support Files/Pods-RokuRemoteApp/Pods-RokuRemoteApp-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/OHHTTPStubs/OHHTTPStubs.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CocoaAsyncSocket/CocoaAsyncSocket.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/OHHTTPStubs/OHHTTPStubs.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CocoaAsyncSocket/CocoaAsyncSocket.framework"
 fi
